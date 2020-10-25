c250a57317c152ecee9c238366d2600d178d0450
Lars Bekhof <larsbekhof@gmail.com>
2020-10-25 11:47:33 +0100

    no message

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # MDServe
-MDService is a tool to view markdown files in your browser.
+MDServe is a tool to view markdown files in your browser with hot reloading!
 
 ## Usage
 `mdserve`
 
 `mdserve --port=3000`
 
-`mdserve --dialect=Maruku`
-
 `mdserve --stylesheet=https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css`
 
-Your markdown files will be available by passing the path on your machine as the URL path.
+Your markdown files will be available by passing the path on your machine as the URL path. Edit your markdown file and as soon as you hit safe the window will refresh!
 
 Example:
 `http://localhost:3000/home/lars/Documents/stuff-i-like.md`
--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,18 @@
 #! /usr/bin/env node
 
-const express = require('express');
-const markdown = require('markdown').markdown;
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const { Converter } = require('showdown');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+// Get CLI arguments
 const inputs = process.argv.slice(2);
 
 const {
 	port,
-	dialect,
 	stylesheet,
 } = inputs.reduce((defaults, option) => {
 	const [optionFlag, value] = option.split('=')
@@ -20,26 +23,41 @@ const {
 	return defaults;
 }, {
 	port: 3000,
-	dialect: 'Maruku',
 	stylesheet: 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css',
 });
 
-const dom = cheerio.load('<html><head></head><body class="markdown-body"></body></html>');
+// Generating dom
+const dom = cheerio.load('<html><head></head><body><div class="markdown-body"></div></body></html>');
 const style = `<link rel="stylesheet" href="${stylesheet}">`;
+const client = `
+	<script src="/socket.io/socket.io.js"></script>
+	<script>
+		const socket = io();
+		socket.on('reload', () => location.reload());
+	</script>
+`;
 dom('head').append(style);
+dom('body').append(client);
 
-const app = express();
+const mdConverter = new Converter();
+mdConverter.setFlavor('github');
 
-app.get('*', (req, res) => {
-	fs.readFile(req.path, (err, data) => {
+app.get(/^((?!socket.io).)*$/, (req, res) => {
+	const path = req.url;
+
+	fs.readFile(path, (err, data) => {
 		if (err) {
 			res.send(err.toString());
-		} else {
-			dom('body').html(markdown.toHTML(data.toString(), dialect));
-
-			res.send(dom.html());
+			return;
 		}
+
+		dom('.markdown-body').html(mdConverter.makeHtml(data.toString()));
+		res.send(dom.html());
+	});
+
+	io.on('connection', (socket) => {
+		fs.watchFile(path, { interval: 50 }, () => socket.emit('reload'));
 	});
 });
 
-app.listen(port, () => console.log(`Serving markdown at 127.0.0.1:${port}`));
+http.listen(port, () => console.log(`Serving markdown at 127.0.0.1:${port}`));
